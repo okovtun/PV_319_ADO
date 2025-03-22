9ac90c28b6873cc1d95dd2d43493b899e482699f
User <User@DESKTOP-0TCDSR6>
2025-03-22 20:36:51 +0300

    INSERT DONE

--- ADO.NET/ADO.NET/Program.cs
+++ ADO.NET/ADO.NET/Program.cs
@@ -59,7 +59,8 @@ namespace ADO.NET
 
 			//Connector.Select("*", "Directors");
 			//Connector.Select("title,release_date,FORMATMESSAGE(N'%s %s',first_name,last_name)", "Movies,Directors", "director=director_id");
-			Connector.InsertDirector("George", "Matrin");
+			//Connector.InsertDirector("Brian", "De Palma");
+			Connector.InsertMovie("Terminator 5 - Genesis", "2015-06-22", "1");
 			Connector.SelectDirectors();
 			Connector.SelectMovies();
 		}
