80d6235190d3fc9fc0f75cc0c42175703d929579
User <User@DESKTOP-0TCDSR6>
2025-04-15 22:20:17 +0300

    Dependecies almost DONE TODO: AllGroups in ComboBox;

--- ADO.NET/Academy/MainForm.cs
+++ ADO.NET/Academy/MainForm.cs
@@ -19,6 +19,8 @@ namespace Academy
 		public Dictionary<string, int> d_directions;
 		public Dictionary<string, int> d_groups;
 
+		public Dictionary<ComboBox, List<ComboBox>> d_dependencies;
+
 		DataGridView[] tables;
 		Query[] queries = new Query[]
 			{
@@ -57,6 +59,11 @@ namespace Academy
 		public MainForm()
 		{
 			InitializeComponent();
+
+			d_dependencies = new Dictionary<ComboBox, List<ComboBox>>()
+			{
+				{ cbStudentsDirection, new List<ComboBox>(){ cbStudentsGroup } }
+			};
 
 			tables = new DataGridView[]
 			{
@@ -186,14 +193,21 @@ namespace Academy
 
 			#region Filtercb_StudentsGroup
 			//Фильтруем выпадающий список групп на вкладке 'Students':
-			Dictionary<string, int> d_groups = connector.GetDictionary
-				(
-				"group_id,group_name",
-				"Groups",
-				i == 0 ? "" : $"[{cb_suffix.ToLower()}]={dictionary[(sender as ComboBox).SelectedItem.ToString()]}"
-				);
-			cbStudentsGroup.Items.Clear();
-			cbStudentsGroup.Items.AddRange(d_groups.Select(g => g.Key).ToArray());
+			//Dictionary<string, int> d_groups = connector.GetDictionary
+			//	(
+			//	"group_id,group_name",
+			//	"Groups",
+			//	i == 0 ? "" : $"[{cb_suffix.ToLower()}]={dictionary[(sender as ComboBox).SelectedItem.ToString()]}"
+			//	);
+			//cbStudentsGroup.Items.Clear();
+			//cbStudentsGroup.Items.AddRange(d_groups.Select(g => g.Key).ToArray());
+			if (d_dependencies.ContainsKey(sender as ComboBox))
+			{
+				foreach (ComboBox cb in d_dependencies[sender as ComboBox])
+				{
+					GetDependentData(cb, sender as ComboBox);
+				}
+			}
 			//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
 			#endregion
 
@@ -203,6 +217,34 @@ namespace Academy
 			if (query.Condition == "") query.Condition = condition;
 			else if(condition != "") query.Condition += $" AND {condition}";
 			LoadPage(tabControl.SelectedIndex, query);
+		}
+		void GetDependentData(ComboBox dependent, ComboBox determinant)
+		{
+			Console.WriteLine("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+			Console.WriteLine(dependent.Name + "\t" + determinant.Name);
+			string dependent_root = 
+				dependent.Name.Substring(Array.FindLastIndex<char>(dependent.Name.ToCharArray(), Char.IsUpper));
+			string determinant_root = 
+				determinant.Name.Substring(Array.FindLastIndex<char>(determinant.Name.ToCharArray(), Char.IsUpper));
+
+			Dictionary<string, int> dictionary =
+				connector.GetDictionary
+				(
+					$"{dependent_root.ToLower()}_id,{dependent_root.ToLower()}_name",
+					$"{dependent_root}s,{determinant_root}s",
+					determinant.SelectedItem == null || determinant.SelectedIndex <= 0 ? "" : $"{determinant_root.ToLower()}={determinant.SelectedIndex}"
+					);
+			foreach (KeyValuePair<string, int> d in dictionary)
+			{
+				Console.WriteLine($"{d.Value}\t{d.Key}");
+			}
+
+			dependent.Items.Clear();
+			dependent.Items.AddRange(dictionary.Select(d => d.Key).ToArray());
+
+			Console.WriteLine("Dependent:  \t"+dependent_root);
+			Console.WriteLine("Determinant:\t"+determinant_root);
+			Console.WriteLine("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
 		}
 	}
 }
